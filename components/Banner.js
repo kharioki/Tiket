9ab4ac3f7ada2794b8ed2d69964ab2acafa7ed1a
tony <antolisah@gmail.com>
2021-10-08 22:47:59 +0300

    banner component

---
+++ components/Banner.js
@@ -0,0 +1,35 @@
+export function Banner({ handleShowModal }) {
+  return (
+    <div className="bg-primary block w-full mb-4 px-2 py-2 md:px-6 rounded-md">
+      <div className="flex-1 flex items-center mb-2">
+        <span className="flex p-2 rounded-md bg-primary">
+          <svg className="h-6 w-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </span>
+
+        <p className="ml-2 text-xs sm:font-sm text-white text-left">
+          Event tickets re-imagined!
+          Create, sell, buy and validate tickets using blockchain technology.
+        </p>
+      </div>
+      <div className="flex-1 flex items-center">
+        <span className="flex p-2 rounded-md bg-primary">
+          <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+          </svg>
+        </span>
+        <p className="ml-2 text-xs sm:font-sm text-white text-left">
+          Coming soon!
+          Resell purchased tickets.
+        </p>
+      </div>
+      <button
+        className="p-2 md:p-4 text-white text-xs sm:text-md border-white border-2 hover:text-primary hover:bg-white tracking-wider rounded-full"
+        onClick={() => handleShowModal()}
+      >
+        Create Tickets
+      </button>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import { tickets } from '../utils/tickets';
 import { EventCard } from '../components/EventCard';
 import { Modal } from '../components/Modal';
+import { Banner } from '../components/Banner';
 
 export default function HomePage() {
   const [showModal, setShowModal] = useState(false);
@@ -23,14 +24,9 @@ export default function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center w-full flex-1 sm:px-12 xl:px-24 sm:py-6 text-center">
-        <button
-          className="p-2 md:p-4 text-primary text-xs sm:text-md border-primary border-2 hover:text-white hover:bg-primary tracking-wider rounded-full"
-          onClick={() => handleShowModal()}
-        >
-          Create Tickets
-        </button>
-        <div className="w-full mb-2 px-2 md:px-6">
+      <main className="flex flex-col items-center w-full flex-1 px-2 sm:px-12 xl:px-24 sm:py-6 text-center">
+        <Banner handleShowModal={handleShowModal} />
+        <div className="w-full mb-2 md:px-6">
           <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-4">
             {tickets.map((ticket, index) => (
               <EventCard key={index} ticket={ticket} />

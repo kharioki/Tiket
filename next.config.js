f8f12f78263f988b6da6b131c01856d642b730f3
tony <antolisah@gmail.com>
2021-10-25 01:34:54 +0300

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
-// module.exports = {
-//   webpack5: false,
-//   webpack: (config) => {
-//     config.node = {
-//       dns: "mock",
-//       fs: "empty",
-//       path: true,
-//       url: false,
-//       net: "empty",
-//     };
-//     return config;
-//   },
-// };
-
 module.exports = {
+  webpack5: false,
   webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      child_process: false,
-      readline: false,
+    config.node = {
+      dns: "mock",
+      fs: "empty",
+      path: true,
+      url: false,
+      net: "empty",
     };
     return config;
   },
 };
+
+// module.exports = {
+//   webpack: (config) => {
+//     config.resolve.fallback = {
+//       ...config.resolve.fallback,
+//       fs: false,
+//       net: false,
+//       child_process: false,
+//       readline: false,
+//     };
+//     return config;
+//   },
+// };

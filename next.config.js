7059d0956fbc1a39af4a06f5f170fb84b76e8839
tony <antolisah@gmail.com>
2021-10-25 04:52:50 +0300

    github workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  assetPrefix: isProd ? '/tiket/' : '',
   webpack5: false,
   webpack: (config) => {
     config.node = {
